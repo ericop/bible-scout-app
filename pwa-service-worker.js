aafbdf64785e4eab0f8431a9e0376643b8f9e2b0
Eric D Johnson <johns7ed@cmich.edu>
2019-07-31 13:08:38 -0400

    " => ', and remove ';"s

--- pwa-service-worker.js
+++ pwa-service-worker.js
@@ -1,47 +1,47 @@
-// This is the "Offline copy of pages" service worker
+// This is the 'Offline copy of pages' service worker
 
-const CACHE = "pwabuilder-offline";
+const CACHE = 'pwabuilder-offline'
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
-const offlineFallbackPage = "index.html";
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = 'index.html'
+const offlineFallbackPage = 'index.html'
 
 // Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener("install", function (event) {
-  console.log("[PWA] Install Event processing");
+self.addEventListener('install', function (event) {
+  console.log('[PWA] Install Event processing')
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("[PWA] Cached offline page during install");
+      console.log('[PWA] Cached offline page during install')
 
-      if (offlineFallbackPage === "index.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
+      if (offlineFallbackPage === 'index.html') {
+        return cache.add(new Response('TODO: Update the value of the offlineFallbackPage constant in the serviceworker.'))
       }
       
-      return cache.add(offlineFallbackPage);
+      return cache.add(offlineFallbackPage)
     })
-  );
-});
+  )
+})
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) {
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return
 
   event.respondWith(
     fetch(event.request)
       .then(function (response) {
-        console.log("[PWA] add page to offline cache: " + response.url);
+        console.log('[PWA] add page to offline cache: ' + response.url)
 
         // If request was success, add or update it in the cache
-        event.waitUntil(updateCache(event.request, response.clone()));
+        event.waitUntil(updateCache(event.request, response.clone()))
 
-        return response;
+        return response
       })
       .catch(function (error) {        
-        console.log("[PWA] Network request Failed. Serving content from cache: " + error);
-        return fromCache(event.request);
+        console.log('[PWA] Network request Failed. Serving content from cache: ' + error)
+        return fromCache(event.request)
       })
-  );
-});
+  )
+})
 
 function fromCache(request) {
   // Check to see if you have it in the cache
@@ -50,16 +50,16 @@ function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
+        return Promise.reject('no-match')
       }
 
-      return matching;
-    });
-  });
+      return matching
+    })
+  })
 }
 
 function updateCache(request, response) {
   return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
+    return cache.put(request, response)
+  })
 }
