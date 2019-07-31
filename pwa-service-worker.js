51a2d994ed772eeaf1b6d8387cb58c2f1ed3ac62
Eric D Johnson <johns7ed@cmich.edu>
2019-07-31 13:05:06 -0400

    avoidCachingPaths http*

--- pwa-service-worker.js
+++ pwa-service-worker.js
@@ -19,6 +19,7 @@ const networkFirstPaths = [
 const avoidCachingPaths = [
     /* Add an array of regex of paths that shouldn't be cached */
     // Example: /\/api\/.*/
+    /http*/
 ]
 
 function pathComparer(requestUrl, pathRegEx) {
