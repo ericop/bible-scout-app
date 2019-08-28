d3cf11e5b9ebc20a85929bb023305b044a3205a8
ericop <eric.op.johnson@gmail.com>
2019-08-28 00:29:30 -0400

    less dark

--- components/reading-card.component.js
+++ components/reading-card.component.js
@@ -176,7 +176,7 @@ const ReadingCardComponent = () => {
                                         }),
                                         m('.row',
                                             m('.col s12 m8', [
-                                                m('button.btn-floating.waves-effect.waves-light.deep-purple.darken-4',
+                                                m('button.btn-floating.waves-effect.waves-light.deep-purple.darken-3',
                                                     {
                                                         disabled: isLoading,
                                                         class: isPlayingAudio ? 'hide' : '',
