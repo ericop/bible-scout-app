2a7760dab1d48928e463dc55ee4aafaff7c40936
ericop <eric.op.johnson@gmail.com>
2019-08-28 00:04:47 -0400

    network-or-cache service worker

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+var CACHE = 'network-or-cache'
+// from https://serviceworke.rs/strategy-network-or-cache_service-worker_doc.html
+self.addEventListener('install', function (evt) {
+  console.log('The service worker is being installed.')
+
+  evt.waitUntil(preCache())
+})
+
+self.addEventListener('fetch', function (evt) {
+  console.log('The service worker is serving the asset.')
+  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+    return fromCache(evt.request)
+  }))
+})
+
+function preCache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      './controlled.html',
+      './asset'
+    ])
+  })
+}
+
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout)
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId)
+      fulfill(response)
+    }, reject)
+  })
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match')
+    })
+  })
+}
